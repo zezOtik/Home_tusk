58617f1a30a3689a274cd24feec21ced5c1b1100
zezOtik <wzomzot@gmail.com>
2016-12-11 14:59:21 +0300

    Vol_3_for_test

--- check dicti.py
+++ check dicti.py
@@ -19,8 +19,6 @@ def generate_lab_step_one(labirint_main,labirint_posetil,labirint_ne_posetil,siz
     labirint_ne_posetil.pop(0)
 
     labirint_posetil[0] = 1
-
-    s = []#переменная для считывания значений функции
     
     i = 0#старт работы алгоритма
     j = 0
@@ -28,9 +26,7 @@ def generate_lab_step_one(labirint_main,labirint_posetil,labirint_ne_posetil,siz
     
     while(i != size_n * size_m - 1 ):#пока не равны значения работать
         #проверка на возможную ошибку 
-        s = randomstep(labirint_posetil,i,size_n,size_m)
-        i = int(s[0])
-        j = int(s[1])
+        (i,j) = randomstep(labirint_posetil,i,size_n,size_m)
         if j != 0:           
             brok_wall(labirint_main, size_n, size_m, i, j)
             labirint_posetil[i] = 1
@@ -43,29 +39,29 @@ def generate_lab_step_one(labirint_main,labirint_posetil,labirint_ne_posetil,siz
     
 
 
-def step_2(labirint,feiklab,labirint_ex,size_n,size_m):
-    s = []
-    if kolvo_elem(labirint_ex) != 0:
+def step_2(labirint,feiklab,labirint_ex,size_n,size_m):#Шаг 2 от прошедших вершин идем
+
+    if kolvo_elem(labirint_ex) != -1:
         
         random_kol = math.trunc(math.sqrt(kolvo_elem(feiklab)))
-
+        
         while(random_kol != 0):
+
             i = randominfeik(feiklab)
             
             i = proverka_sosed(feiklab,i,size_n,size_m)
 
             while(i != -1):
-                
-                s = randomstep(feiklab,i,size_n,size_m)
-                i = int(s[0])
-                j = int(s[1])
-
-                brok_wall(labirint, size_n, size_m, i, j)
-
                 feiklab[i] = 1
+                
+                (i,j) = randomstep(feiklab,i,size_n,size_m)
+             
+
+                labirint = brok_wall(labirint, size_n, size_m, i, j)
 
                 if labirint_ex.get(i) == None:
-                    return 1
+
+                    break
                 else:
                     labirint_ex.pop(i)
                 i = proverka_sosed(feiklab,i,size_n,size_m)
@@ -75,19 +71,37 @@ def step_2(labirint,feiklab,labirint_ex,size_n,size_m):
         return labirint,feiklab,labirint_ex
 
 
-#def step_3(labirint,feiklab,labirint_ex,size_n,size_m):
-    
+def step_3(labirint,feiklab,labirint_ex,size_n,size_m):
 
+    while (kolvo_elem(labirint_ex) != -1):
 
-def kolvo_elem(hashtable):
-    kol_vo = len(list(hashtable.keys()))-1
+        i =  randominfeik(labirint_ex)
+ 
+        labirint_ex.pop(i)
+        while(feiklab.get(i) == None):
+            feiklab[i] = 1
+            (i,j) = randomstep_for_last(i,size_n,size_m)
+            labirint = brok_wall(labirint,size_n,size_m,i,j)
+            
+            if labirint_ex.get(i) == None:
+                break
+            else:
+                labirint_ex.pop(i)            
+    return labirint
+      
 
-    return kol_vo
+def kolvo_elem(hashtable):#получение количество значений в хэш-таблице
+    kol_vo = len(list(hashtable.keys()))
+    if kol_vo == 0:
+        kol_vo = -1
+        return kol_vo
+    else:
+        return kol_vo
     
 
 
 def randominfeik(feiklab):#Вывод рандомной уже посещенной клетки
-    p = random.randint(0,len(list(feiklab.keys()))-1)
+    p = random.randint(0,kolvo_elem(feiklab)-1)
     i = list(feiklab.keys())[p]
     return i
 
@@ -235,7 +249,102 @@ def randomstep(feiklab,i,n,m):#По входящим данным выбирае
             i = if_I(i,n,m,j)
             return i,j
     
+def randomstep_for_last(i,n,m):#По входящим данным выбирает куда следует стрелка пробивания стенок, для шага 3
+    
+    #проверил(0,1,1,1)
+    if (i % m == 0) and (i - m >= 0) and (i % m != m-1) and (i+m <= n*m -1):
+            j = random.randint(2,4)
+            i = if_I(i,n,m,j)
+            return i,j
+        
+    #wp(0,0,0,1)
+    elif (i % m == 0) and (i - m < 0) and (i % m == m-1) and (i+m <= n*m -1):
+            j = 4
+            i = if_I(i,n,m,j)
+            return i,j
+        
+    #wp(0,1,0,1)   
+    elif (i % m == 0) and (i - m >= 0) and (i % m == m-1) and (i + m <= n*m -1):
+            j = random.choice([2,4])
+            i = if_I(i,n,m,j)
+            return i,j
 
+    #(0,0,1,0)
+    elif (i % m == 0) and (i - m < 0) and (i % m != m-1) and (i + m > n*m -1):
+            j = 3
+            i = if_I(i,n,m,j)
+            return i,j
+        
+    #(0,1,0,0)
+    elif (i % m == 0) and (i - m >= 0) and (i % m == m-1) and (i+m > n*m -1):
+            j = 2
+            i = if_I(i,n,m,j)
+            return i,j
+        
+    #(0,1,1,0)
+    elif (i % m == 0) and (i - m >= 0) and (i % m != m-1) and (i + m > n*m -1):
+            j = random.randint(2,3)
+            i = if_I(i,n,m,j)
+            return i,j
+        
+    #(0,0,1,1)
+    elif (i % m == 0) and (i - m < 0) and (i % m != m-1) and (i + m <= n*m -1):
+            j = random.randint(3,4)
+            i = if_I(i,n,m,j)
+            return i,j
+
+    #(1000)
+    elif (i % m != 0) and (i - m < 0) and (i % m == m-1) and (i + m > n*m -1):
+            j = 1
+            i = if_I(i,n,m,j)
+            return i,j
+            
+        
+    #1001
+    elif (i % m != 0) and (i - m < 0) and (i % m == m-1) and (i+m <= n*m -1):
+            j = random.choice([1,4])
+            i = if_I(i,n,m,j)
+            return i,j
+        
+        
+    #(1010)
+    elif (i % m != 0) and (i - m < 0) and (i % m != m-1) and (i+m > n*m -1):
+            j = random.choice([1,3])
+            i = if_I(i,n,m,j)
+            return i,j
+
+
+    #(1011)
+    elif (i % m != 0) and (i - m < 0) and (i % m != m-1) and (i + m <= n*m -1):
+            j = random.choice([1,3,4])
+            i = if_I(i,n,m,j)
+            return i,j
+        
+
+    #(1100)
+    elif (i % m != 0) and (i - m >= 0) and (i % m == m-1) and (i+m > n*m -1):
+            j = random.randint(1,2)
+            i = if_I(i,n,m,j)
+            return i,j
+        
+        
+    #(1101)
+    elif (i % m != 0) and (i - m >= 0) and (i % m == m-1) and (i+m <= n*m -1):
+            j = random.choice([1,2,4])
+            i = if_I(i,n,m,j)
+            return i,j
+            
+        
+    #(1110)
+    elif (i % m != 0) and (i - m >= 0) and (i % m != m-1) and (i+m > n*m -1):
+            j = random.randint(1,3)
+            i = if_I(i,n,m,j)
+            return i,j
+    #(1111)
+    elif (i % m != 0) and (i - m >= 0) and (i % m != m-1) and (i+m <= n*m -1):
+            j = random.randint(1,3)
+            i = if_I(i,n,m,j)
+            return i,j
 
 
 def brok_wall(labirint,n,m,i,p):#Пробивание лабиринта
@@ -337,23 +446,29 @@ def proverka_sosed(feiklab,i,n,m):
         return i
     else:
         return i
+
     
 def main():
     file = "Enter.txt"
 
     #receipt our params
-    size_n = port(file)[0]
-    size_m = port(file)[1]
+    (size_n, size_m) = port(file)
 
-    labirint_main = dict()#создали хэш таблицу
+    labirint_main = dict()#создали хэш таблицу основная
     labirint_main = {i: 11 for i in range(size_n*size_m)}#забили ее 11
 
-    labirint_ne_posetil = dict()
+    labirint_ne_posetil = dict()#хэш-табл не посещенных
     labirint_ne_posetil = {i: 1 for i in range(size_n*size_m)}
 
     labirint_posetil = dict()#хэш-таблица для посещенных
    
-    print(generate_lab_step_one(labirint_main,labirint_posetil,labirint_ne_posetil,size_n, size_m))
+    (labirint_main,labirint_posetil,labirint_ne_posetil) = (generate_lab_step_one(labirint_main,labirint_posetil,labirint_ne_posetil,size_n, size_m))
+
+    (labirint_main,labirint_posetil,labirint_ne_posetil) = (step_2(labirint_main,labirint_posetil,labirint_ne_posetil,size_n,size_m))
+    
+    labirint_main = step_3(labirint_main,labirint_posetil,labirint_ne_posetil,size_n,size_m)
+
+    print(labirint_main)
     
 
   
